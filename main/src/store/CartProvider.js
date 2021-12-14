e8849fcbb6e18e90bc90ff9113564c2acaa80ce6
Tunc <tuncgonelfb@hotmail.com>
2021-12-13 23:56:04 -0500

    Cart and State

---
+++ main/src/store/CartProvider.js
@@ -0,0 +1,49 @@
+import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCart = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cardReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "REM") {
+  }
+  return defaultCart;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cardReducer, defaultCart);
+
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "REM", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemHandler,
+    removeItem: removeItemHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
